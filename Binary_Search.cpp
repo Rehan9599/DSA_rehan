928fef43ca86316229c5874dcef6dde63214e992
Rehan fazal <fazalrehan091@gmail.com>
2025-09-04 16:39:52 +0530

    BS DOen

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -179,48 +179,136 @@ int countOccurrences(vector<int>& nums, int target) {
 
 
 int searchRot(vector<int> &nums, int target) {
-    int maxi=0;
-        for(auto x: nums){
-            maxi=max(maxi,x);
-        }
-        cout<< maxi<<  "\n";
-    vector<pair<int ,int>> valueInd(maxi+1,{-1,-1});
-    for(int i=0; i<nums.size(); i++){
-        valueInd[nums[i]]={nums[i],i};
-    }
-    for(int i=0; i<maxi; i++){
-        if(valueInd[i+1].first!=-1){
-            if(i==maxi) continue;
-                valueInd[i].first=valueInd[i+1].first-1;
-            }
-            else{
-                if(i==0) continue;
-                valueInd[i].first=valueInd[i-1].first+1;
-            }
-        }
+    // int maxi=0;
+    //     for(auto x: nums){
+    //         maxi=max(maxi,x);
+    //     }
+    //     cout<< maxi<<  "\n";
+    // vector<pair<int ,int>> valueInd(maxi+1,{-1,-1});
+    // for(int i=0; i<nums.size(); i++){
+    //     valueInd[nums[i]]={nums[i],i};
+    // }
+    // for(int i=0; i<maxi; i++){
+    //     if(valueInd[i+1].first!=-1){
+    //         if(i==maxi) continue;
+    //             valueInd[i].first=valueInd[i+1].first-1;
+    //         }
+    //         else{
+    //             if(i==0) continue;
+    //             valueInd[i].first=valueInd[i-1].first+1;
+    //         }
+    //     }
 
-    for(auto x: valueInd){
-        cout<< x.first<< ","<< x.second<<"\n";
-    }
-    int low=0, high=valueInd.size()-1;
+    // for(auto x: valueInd){
+    //     cout<< x.first<< ","<< x.second<<"\n";
+    // }
+    // int low=0, high=valueInd.size()-1;
+    // while(low<=high){
+    //     int mid= (low+high)/2;
+    //     if(target>valueInd[mid].first){
+    //         low=mid+1;
+    //     }
+    //     else if(target<valueInd[mid].first){
+    //     high=mid-1;
+    //     }
+    //     else if(target==valueInd[mid].first){
+    //         return valueInd[mid].second;
+    //     }
+    // }
+    // return -1;
+
+
+ int low=0, high=nums.size()-1;
     while(low<=high){
         int mid= (low+high)/2;
-        if(target>valueInd[mid].first){
-            low=mid+1;
+        if(target==nums[mid]){
+            return mid;
         }
-        else if(target<valueInd[mid].first){
-        high=mid-1;
+        if(nums[high]>=nums[mid]){
+             if(nums[mid]<=target && target<=nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-        else if(target==valueInd[mid].first){
-            return valueInd[mid].second;
+        else if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && target<=nums[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
+        
+    }
+return -1;
+
+}
+
+
+
+int findMin(vector<int> &nums)  {
+    int low=0, high=nums.size()-1,mini=INT16_MAX;
+     while(low<=high){
+        int mid= (low+high)/2;
+        if(nums[low]<=nums[mid]){
+             mini=min(mini,nums[low]);
+             low=mid+1;
+        }
+        else{
+            mini=min(mini,nums[mid]);
+            high=mid-1;
+        }
+    }
+    return  mini;
+}
+
+
+int findKRotation(vector<int> &nums)  {
+    int f=findMin(nums);
+    for(int i=0;i<nums.size(); i++){
+        if(nums[i]==f) return i;
+    }
+}
+
+
+
+int singleNonDuplicate(vector<int> &nums) {
+    int low=0, high=nums.size()-1;
+     while(low<=high){
+        int mid=(low+high)/2;
+        cout<<low<<','<<mid<<","<<high<<"\n";
+        if((nums.size()-1)/2 % 2==0){
+             if(nums[mid]==nums[mid-1]){
+            high=mid+1;
+        }
+        else if(nums[mid]==nums[mid+1]){
+            low=mid-1;
+        }
+        else{
+            return nums[mid];
+        }
+        }
+        else{
+            if(nums[mid]==nums[mid-1]){
+            low=mid-1;
+        }
+        else if(nums[mid]==nums[mid+1]){
+            high=mid+1;
+        }
+        else{
+            return nums[mid];
+        }
+        } 
     }
     return -1;
 }
 
 
+
 int main(){
-    vector<int> nums={1,3,5};
-    cout<<searchRot(nums,3);
+    vector<int> nums={1};
+    cout<<singleNonDuplicate(nums);
     return 0;
 }
