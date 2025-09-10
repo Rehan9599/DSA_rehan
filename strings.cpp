3a6d70dcfd33f376d7e350f5816d35c0893e0392
Rehan fazal <fazalrehan091@gmail.com>
2025-09-10 06:58:50 +0530

    prefix

--- strings.cpp
+++ strings.cpp
@@ -72,9 +72,54 @@ string binaryToHexa(string s){
     return zeroes;
 }
 
+
+string largeOddNum(string& s){
+    int l=s.size();
+    string oddS;
+    int oddLargest=INT16_MIN;
+    for(int i=0;i<l;i++){
+        if(s[i]=='0') continue;
+        for(int j=i; j<l; j++){
+            oddS=s.substr(i,j-i+1);
+            int l1=oddS.size();
+            char lastDigit=oddS.back();
+            int dig=stoi(oddS);
+            if( ( lastDigit - '0') %2 !=0 ){
+                oddLargest=max(oddLargest,dig);
+            }
+        }
+    }
+
+    string ans=to_string(oddLargest);
+    return ans;
+}
+
+
+string longestCommonPrefix(vector<string>& str){
+    string ans="";
+    int count=0;
+    int j;
+    for(int i=1; i<str.size(); i++){
+        if(str[0].front()!=str[i].front()){
+                return ans;
+            }
+        for(j=1; j<str.size(); j++){
+            if(str[0].substr(0,j)!=str[i].substr(0,j)){
+                break;
+            }
+            else{
+                count++;
+            }
+    }
+}
+    if(count==str.size()-1) ans.append(str[0].substr(0,j));
+    return ans;
+}
+
+
 int main(){
-    string s="1010101011";
-    cout<<binaryToHexa(s);
+    vector<string> str={"flowers" , "flow" , "fly", "flight"};
+    cout<<longestCommonPrefix(str);
 
     return 0;
 }
