4616d20d4d5892058a668e257a421320c5cd0cdb
rehan9599 <fazalrehan091@gmail.com>
2026-02-21 07:31:37 +0530

    maxsubstr

--- slidingWindow&@pointer.cpp
+++ slidingWindow&@pointer.cpp
@@ -17,23 +17,21 @@ int longestNonRepeatingSubstring(string& s){
     int l=0;
     int r=0;
     int longest=0;
-    string ss;
-    while(r<s.size()-1){
-        ss=s.substr(l,r);
-        cout<<ss<<" "<<longest<<"\n";
-        for(int i=0; i<ss.size();i++){
-            if(ss[i]==ss[ss.size()-i]/2){
-                l++;
-            }
+    map<char,int> mpstr;
+    while(r<s.size()){
+        if(mpstr.find(s[r]) != mpstr.end() && mpstr[s[r]]>=l){
+            l=mpstr[s[r]]+1;
         }
+        mpstr[s[r]]=r;
         longest=max(longest,r-l+1);
+        cout<<s.substr(l,r-l+1)<<" "<<mpstr[s[r]]<<" "<<longest<<" "<<l<<" "<<r<<"\n";
         r++;
     }
     return longest;
 }
 
 int main(){
-    string S = "abcddabac";
+    string S = "aaabbbccc";
     cout<<longestNonRepeatingSubstring(S);
     return 0;
 }
