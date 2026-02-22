95a4047a08515617c109b93feec11a033de48150
rehan9599 <fazalrehan091@gmail.com>
2026-02-22 23:35:00 +0530

    heappppp

---
+++ heap.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <utility>
+#include <list>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <map>
+#include <string>
+using namespace std;
+
+class Solution{
+    public:
+
+        void initializeHeap(){
+
+        }
+
+        void insert(int key){
+            
+        }
+
+        void changeKey(int index, int new_val){
+            
+        }
+
+        void extractMin(){
+            
+        }
+
+        bool isEmpty(){
+            
+        }
+
+        int getMin(){
+            
+        }
+
+        int heapSize(){
+            
+        }
+};
+
+int main(){
+
+    return 0;
+}
