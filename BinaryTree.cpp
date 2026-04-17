17d176c95241245d0a24f7b8ed4a47db9d4c1703
rehan9599 <fazalrehan091@gmail.com>
2026-04-17 07:38:25 +0530

    tree stratted

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -15,9 +15,46 @@
 using namespace std;
 
 
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+};
 
-int main(){
+vector<vector<int>> treeTraversal(TreeNode* root){
+    vector<int> preOrder;
+    vector<int> posOrder;
+    vector<int> inOrder;
+    vector<vector<int>> res;
 
     
+
+
+}
+
+int main(){
+    vector<int> roots = {1, 3, 4, 5, 2, 7, 6 };
+
+    vector<TreeNode*> root(roots.size(),nullptr);
+
+    for(int i=0;i<roots.size();i++){
+        root[i]=new TreeNode(roots[i]);
+    }
+
+    for(int i=0;i<roots.size();i++){
+        int l=2*i+1;
+        int r= 2*i+2;
+
+        if(r<roots.size()) root[i]->right=root[r];
+        if(l<roots.size()) root[i]->left=root[l];
+    }
+
+    for(auto x: treeTraversal(root[0])){
+        for(auto y: x){
+            cout<<y<<" ";
+        }
+        cout<<"\n";
+    }
     return 0;
 }
--- nsl.cpp
+++ nsl.cpp
@@ -3,32 +3,102 @@
 #include <iomanip>
 
 using namespace std;
-
-double f(double x){
-   return (pow(x,3)-2*x-5);
-}
-
-double df(double x){
-   return (3*pow(x,2)-2);
-}
-
-
-double findRootNewton(double x0, double x1, double e){
-      static int iter = 1;
-      cout << "Iteration " << iter << ": x = " << x1 << ", f(x) = " << f(x1) << endl;
-      if(fabs(x1-x0) <= e){
-         return x1;
+void gaussJordan(double a[10][11], int n, double x[10]){
+   for(int i = 0; i < n; i++){
+      if(fabs(a[i][i]) < 1e-12){
+         for(int k = i + 1; k < n; k++){
+            if(fabs(a[k][i]) > 1e-12){
+               for(int j = 0; j <= n; j++){
+                  swap(a[i][j], a[k][j]);
+               }
+               break;
+            }
+         }
       }
-      double x2 = x1 - f(x1)/df(x1);
-      iter++;
-      return findRootNewton(x1, x2, e);
+
+      double pivot = a[i][i];
+      for(int j = 0; j <= n; j++){
+         a[i][j] = a[i][j] / pivot;
+      }
+
+      for(int k = 0; k < n; k++){
+         if(k != i){
+            double factor = a[k][i];
+            for(int j = 0; j <= n; j++){
+               a[k][j] = a[k][j] - factor * a[i][j];
+            }
+         }
+      }
+   }
+
+   for(int i = 0; i < n; i++){
+      x[i] = a[i][n];
+   }
 }
+
 
 
 int main(){
-    cout << "Rehan Fazal 24BCS067 \n";
-   double x0 = 2.0;
-   double x1 = x0 - f(x0)/df(x0);
-   cout << findRootNewton(x0, x1, 0.0001);
+   cout << "Rehan Fazal 24BCS067 \n";
+
+   int n;
+   double a1[10][11], a2[10][11];
+   double x1[10], x2[10];
+
+   cout << "Enter number of equations: ";
+   cin >> n;
+
+   cout << "Enter augmented matrix coefficients:" << endl;
+   for(int i = 0; i < n; i++){
+      for(int j = 0; j <= n; j++){
+         cin >> a1[i][j];
+         a2[i][j] = a1[i][j];
+      }
+   }
+
+   cout << "\nGauss Jordan Process:\n";
+   gaussJordan(a2, n, x2);
+   cout << "Solution by Gauss Jordan:" << endl;
+   for(int i = 0; i < n; i++){
+      cout << "x" << i + 1 << " = " << fixed << setprecision(6) << x2[i] << endl;
+   }
    return 0;
 }
+
+// cout << "\nGauss Elimination Process:\n";
+   // gaussElimination(a1, n, x1);
+   // cout << "Solution by Gauss Elimination:" << endl;
+   // for(int i = 0; i < n; i++){
+   //    cout << "x" << i + 1 << " = " << fixed << setprecision(6) << x1[i] << endl;
+   // }
+
+   
+   void gaussElimination(double a[10][11], int n, double x[10]){
+//    for(int k = 0; k < n - 1; k++){
+//       if(fabs(a[k][k]) < 1e-12){
+//          for(int i = k + 1; i < n; i++){
+//             if(fabs(a[i][k]) > 1e-12){
+//                for(int j = 0; j <= n; j++){
+//                   swap(a[k][j], a[i][j]);
+//                }
+//                break;
+//             }
+//          }
+//       }
+
+//       for(int i = k + 1; i < n; i++){
+//          double factor = a[i][k] / a[k][k];
+//          for(int j = k; j <= n; j++){
+//             a[i][j] = a[i][j] - factor * a[k][j];
+//          }
+//       }
+//    }
+
+//    for(int i = n - 1; i >= 0; i--){
+//       double sum = 0;
+//       for(int j = i + 1; j < n; j++){
+//          sum = sum + a[i][j] * x[j];
+//       }
+//       x[i] = (a[i][n] - sum) / a[i][i];
+//    }
+// }
